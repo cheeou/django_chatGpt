d13a8eaf2fb5969610ce812bb39e61366dc587f7
cheeou <best.coder.jiwoo@gmail.com>
2024-08-14 15:58:15 +0900

    read requirements file to root directory

--- chatgpt_project/chatgpt_project/settings.py
+++ chatgpt_project/chatgpt_project/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = "django-insecure-2hh%l*5-@zjeu#o46cue%nufztwkqjl=n_&5(lz!jf)%akgsdl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
